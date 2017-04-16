711a785bab6843c5e8ec73ff38d2dfa48526c4d2
seochan2 <seochan2@gmail.com>
2017-04-17 07:11:23 +0900

    - first commit

---
+++ SBSPG/src/main/java/com/seochan2/sbspg/SbspgApplication.java
@@ -0,0 +1,12 @@
+package com.seochan2.sbspg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbspgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbspgApplication.class, args);
+	}
+}
---
+++ SBSPG/src/main/java/com/seochan2/sbspg/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.seochan2.sbspg;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SbspgApplication.class);
+	}
+
+}
