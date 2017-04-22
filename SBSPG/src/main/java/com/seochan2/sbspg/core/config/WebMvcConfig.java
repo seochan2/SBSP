226f582c89dda7e38480a4681bc1fc3c3bddccb2
seochan2 <seochan2@gmail.com>
2017-04-22 17:25:16 +0900

    - InternalResourceViewResolver 설정 추가

--- SBSPG/src/main/java/com/seochan2/sbspg/core/config/WebMvcConfig.java
+++ SBSPG/src/main/java/com/seochan2/sbspg/core/config/WebMvcConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
@@ -17,6 +20,15 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	   public void addInterceptors(InterceptorRegistry registry) {
 	      registry.addInterceptor(localeChangeInterceptor()).addPathPatterns("/**");
 	   }
+	 
+	 @Override
+	    public void configureViewResolvers(ViewResolverRegistry registry) {
+	        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+	        viewResolver.setPrefix("/WEB-INF/views/");
+	        viewResolver.setSuffix(".jsp");
+	        viewResolver.setViewClass(JstlView.class);
+	        registry.viewResolver(viewResolver);
+	    }
 	 
 	 @Bean
 	   public LocaleResolver localeResolver() {
